d5a1e78c3929aefe73e51a701ae753eaa2340f60
CalaSurs <lautileonardolopez@gmail.com>
2026-05-14 15:33:11 -0300

    Delete dist directory

--- dist/server/assets/_tanstack-start-manifest_v-B1l_M3_c.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Cala/Downloads/apex-emporium-main (1)/apex-emporium-main/src/routes/__root.tsx", children: ["/", "/catalog"], assets: void 0, preloads: ["/assets/index-BBBu-9TC.js"] }, "/": { filePath: "C:/Users/Cala/Downloads/apex-emporium-main (1)/apex-emporium-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-D9DMnkui.js", "/assets/PurchaseToasts-BfkkM_kc.js"] }, "/catalog": { filePath: "C:/Users/Cala/Downloads/apex-emporium-main (1)/apex-emporium-main/src/routes/catalog.tsx", children: void 0, assets: void 0, preloads: ["/assets/catalog-DJSUdpwN.js", "/assets/PurchaseToasts-BfkkM_kc.js"] } }, clientEntry: "/assets/index-BBBu-9TC.js" });
-export {
-  tsrStartManifest
-};
--- dist/server/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-CC21re6Y.js";
-import "node:events";
-export {
-  w as default
-};
